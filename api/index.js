7cd2fdd63830d6a44ca68f563b6278ce3014ebf8
Mart <martyanovhi@gmail.com>
2020-04-20 10:07:10 +0300

    try up vps

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Datastore = require('nedb');
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+app.listen(3000, '194.67.92.127');
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
