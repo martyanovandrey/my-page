6e8be7a7a9e0b2059069ac84df00a73c744539b2
Mart <martyanovhi@gmail.com>
2020-04-20 15:58:45 +0300

    vps

--- api/index.js
+++ api/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const Datastore = require('nedb');
-//const ip = '194.67.92.127';
-const ip = '127.0.0.1';
+const ip = '194.67.92.127';
 const port = 80;
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const privateKey   = fs.readFileSync('/etc/ssl/martyanovandrey.key', 'utf8');
+const certificate = fs.readFileSync('/etc/ssl/martyanovandrey.crt', 'utf8');
+
+const options = {key: privateKey, cert: certificate};
 
 const app = express();
-app.listen(port, ip, () => console.log("Listening to "+ip+":"+port));
+const server = https.createServer(options, app);
+
+server.listen(port, ip, () => console.log("Listening to "+ip+":"+port));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-const database = new Datastore('datadase.db');
+const database = new Datastore('database.db');
 database.loadDatabase();
 
 app.get('/api', (request, responce) => {
@@ -31,3 +39,9 @@ app.post('/api', (request, responce) => {
     database.insert(data);
     responce.json(data);
 });
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(options, app);
+
+//httpServer.listen(8080);
+//httpsServer.listen(8443);
